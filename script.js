c9ecae181711357c623d4ffcf4328fcf9696ff4d
Marco <5439724524@student.cms.k12.nc.us>
2025-11-25 12:52:13 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const multiplayer = document.getElementById('multiplayer');
+const titlescreen = document.getElementById('titlescreen');
+
+multiplayer.addEventListener('click', function() {
+  titlescreen.classList.add('hidden');
+});
