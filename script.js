a44adb4f28ff39e940b3951087095ba0b81671d7
Marco <5439724524@student.cms.k12.nc.us>
2025-11-27 17:23:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const multiplayer = document.getElementById('multiplayer');
 const titlescreen = document.getElementById('titlescreen');
 
 multiplayer.addEventListener('click', function() {
-  titlescreen.style.display = "none";
+  titlescreen.style.display = 'none';
 });
