09b2eeea76ce431f7d4720077773e9c805684c67
Marco <5439724524@student.cms.k12.nc.us>
2025-11-28 12:50:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+const server = document.getElementById('multiplayer-menu');
+
+server.style.display = 'none';
+
 document.addEventListener('DOMContentLoaded', (event) => {
 const multiplayer = document.getElementById('multiplayer');
 const titlescreen = document.getElementById('titlescreen');
 
 multiplayer.addEventListener('click', function() {
   titlescreen.style.display = 'none';
+  server.style.display = 'inline';
  });
 }); 
