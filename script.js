6eefc0b046f6472f22e652d115aa95d4c35b7657
Marco <5439724524@student.cms.k12.nc.us>
2025-12-19 08:30:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+const load = document.getElementById('piecraft');
 const server = document.getElementById('multiplayer-menu');
+const multiplayer = document.getElementById('multiplayer');
+const titlescreen = document.getElementById('titlescreen');
 
 server.style.display = 'none';
 
-const multiplayer = document.getElementById('multiplayer');
-const titlescreen = document.getElementById('titlescreen');
+multiplayer.style.display = 'none';
+
+titlescreen.style.display = 'none';
+
+setTimeout(function() {
+  load.style.display = 'none';
+  titlescreen.style.display = 'block';
+  multiplayer.style.display = 'block';
+   }); , 2000);
 
 multiplayer.addEventListener('click', function() {
   titlescreen.style.display = 'none';
